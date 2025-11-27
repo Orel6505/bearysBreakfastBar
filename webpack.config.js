a8e6c37bb6c1a5ad14e22f6432f11a150dcd64a5
Orel Yosupov <orely4242@gmail.com>
2025-11-27 15:18:14 +0200

    Restaurant: Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+// webpack.config.js
+const path = require("node:path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      }, // Rule for CSS files
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      }, // Added rule for HTML files
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      }, // Added rule for image assets
+    ],
+  },
+};
+
+//Nodejs uses CJS instead of ESM for webpack config files
+//So we use require and module.exports instead of import and export
